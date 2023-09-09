516b839136d14ed82c8db4b76486e378a81857f1
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2023-09-09 16:06:48 +0800

    created learn_flutter_page

---
+++ flutter3/lib/learn_flutter_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LearFlutterPage extends StatefulWidget {
+  const LearFlutterPage({super.key});
+
+  @override
+  State<LearFlutterPage> createState() => _LearFlutterPageState();
+}
+
+class _LearFlutterPageState extends State<LearFlutterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
